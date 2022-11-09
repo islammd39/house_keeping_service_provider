aabb8b574480171342f714181048e566a52a0fbe
islammd39e <islammdrahat308@gmail.comm>
2022-11-09 13:03:06 +0600

    upload nav section

--- src/router/Routes.js
+++ src/router/Routes.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main/Main";
+import Blog from "../pages/Blog/Blog";
 import Home from "../pages/Home/Home/Home";
+import Service from "../pages/Service/Service";
 
 export const router = createBrowserRouter([
     {
@@ -10,6 +12,14 @@ export const router = createBrowserRouter([
             {
                 path:"/",
                 element:<Home></Home>
+            },
+            {
+                path: "/service",
+                element:<Service></Service>
+            },
+            {
+                path:"/blog",
+                element:<Blog></Blog>
             }
         ]
     }
--- src/shared/Header/Header.js
+++ src/shared/Header/Header.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/housekeeping.png"
 
 const Header = () => {
-    return (
-        <div>
-            <h2>this is header section</h2>
+  return (
+    <nav>
+      <div className="navbar bg-base-100">
+        <img className=" w-10" src={logo} alt="" />
+        <div className="flex-1">
+          <Link to="/" className="normal-case text-xl">
+          Fresh & Cleaning
+          </Link>
         </div>
-    );
+        <div className="flex-none">
+          <ul className="menu menu-horizontal p-0">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/service">Service</Link>
+            </li>
+            <li>
+              <Link to="/blog">Blog</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
